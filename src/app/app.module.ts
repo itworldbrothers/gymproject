410bf02de7ca68b3aa854c15cba0e6fabf50350b
itworldbrothers <itworldbrothers@outlook.com>
2022-12-15 21:42:41 +0530

    bottom bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BottombarComponent } from './bottombar/bottombar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BottombarComponent
   ],
   imports: [
     BrowserModule,
