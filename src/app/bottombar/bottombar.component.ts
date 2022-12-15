e1856815d80dcb0e147b736e3935d800675d56a2
itworldbrothers <itworldbrothers@outlook.com>
2022-12-15 21:46:04 +0530

    okkk

---
+++ src/app/bottombar/bottombar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottombar',
+  templateUrl: './bottombar.component.html',
+  styleUrls: ['./bottombar.component.css']
+})
+export class BottombarComponent {
+
+}
