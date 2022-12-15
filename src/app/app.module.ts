20c34ca0c8daec3c8aa8b573fa354dd9c8a47b5a
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2022-12-15 22:35:02 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BottombarComponent } from './bottombar/bottombar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material/material.module";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    BottombarComponent
+    BottombarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
