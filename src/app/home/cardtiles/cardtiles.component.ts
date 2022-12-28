dd874048e28cca4075d0ed6bf5fbdd3806b98dd5
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2022-12-28 23:18:39 +0530

    time set

--- src/app/home/cardtiles/cardtiles.component.ts
+++ src/app/home/cardtiles/cardtiles.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CardtilesComponent {
 
+  wish:string = (new Date().getHours() > 3 && new Date().getHours() < 12) ? "Good Morning" : (new Date().getHours() > 12 && new Date().getHours() <= 19) ? "Good AfterNoon" : "Good Night";
+
 }
