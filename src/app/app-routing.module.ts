85bb8d25b296573c7bcae0b9daa78f617f33f36a
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-01-04 22:51:15 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
