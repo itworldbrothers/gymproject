9053af28a22c47869e51615d50c49dc4636e6665
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-01-07 22:24:31 +0530

    new

--- src/app/auth/auth/auth-routing.module.ts
+++ src/app/auth/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from '../login/login.component';
 
