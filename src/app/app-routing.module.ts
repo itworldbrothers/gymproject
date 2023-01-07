6b847371f42c71174082bc2b5d792fa62bc7df81
itworldbrothers <itworldbrothers@outlook.com>
2023-01-07 22:13:24 +0530

    path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo:'auth/auth', pathMatch: "full" },
+  { path: '', redirectTo:'home', pathMatch: "full" },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) },
   { path: 'auth/auth', loadChildren: () => import('./auth/auth/auth.module').then(m => m.AuthModule) }
--- src/app/auth/auth/auth-routing.module.ts
+++ src/app/auth/auth/auth-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 import { AuthComponent } from './auth.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [{ path: '', component: AuthComponent }, 
+{ path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
