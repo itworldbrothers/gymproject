5685b88330fb41ac171e094c23d57f8e41cd9afb
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-01-07 23:16:54 +0530

    new

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { LoginComponent } from '../auth/login/login.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent}];
 
