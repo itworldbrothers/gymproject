a1f8fa701cfbb7b210e01d1e6ac3fae7e4aa6dbb
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-01-07 22:23:14 +0530

    new

--- src/app/auth/auth/auth-routing.module.ts
+++ src/app/auth/auth/auth-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from '../login/login.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [{ path: '', component: AuthComponent},
+{ path: "login", component: LoginComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
