029991fd8ab0165f13373e68d6634e861dcb16d6
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-01-09 22:24:19 +0530

    new

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   hide = true;
+
+ registerForm! : FormGroup;
+
+  constructor( private fb : FormBuilder) {
+   this.registerForm = this.fb.group({
+   name: ['',Validators.required],
+   email: ['',Validators.required],
+   mobile: ['',Validators.required],
+   password: ['',Validators.required],
+   cpassword: ['',Validators.required],
+
+   })
+  }
+
+  
+  ngOnInit(): void {
+    
+  }
+
+  registerdata(){
+  
+   
+  }
+  
 }
